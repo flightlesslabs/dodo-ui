3b79f83d5aaf304352b746cb5c88bbda1a0e2e88
siddacool <sidthecool007@gmail.com>
2025-04-07 10:42:09 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@2731064ac778ebed1efa5012a19698a1f9c0680d 🚀

--- assets/Getting Started-7Lt8uHB6.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-P9OXKMel.js";import{S as i}from"./index-C2jP4H4L.js";import"./index-6otl1p8d.js";import"./iframe-CS3Al0ny.js";import"./index-Daw7QRpi.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function e(n){const o={h1:"h1",h2:"h2",...r(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(o.h1,{id:"getting-started",children:"Getting Started"}),`
-`,t.jsx(o.h2,{id:"install",children:"Install"}),`
-`,t.jsx(i,{dark:!0,language:"bash",code:`
-// pnpm
-pnpm add @siddacoolpkg/dodo-ui
-
-// npm
-npm i @siddacoolpkg/dodo-ui
-`})]})}function h(n={}){const{wrapper:o}={...r(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{h as default};
