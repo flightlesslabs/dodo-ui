79b979d1bfbb933e93685a5b85cb864c4d84f1c3
siddacool <sidthecool007@gmail.com>
2025-04-07 11:29:57 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@9cffa1c9e8406c71c5bf5160dbce2a2b00ca8995 🚀

--- assets/DocsRenderer-CFRXHY34-DIOo9RzH.js
+++ assets/DocsRenderer-CFRXHY34-DIOo9RzH.js
@@ -1,5 +1,5 @@
 const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./index-P9OXKMel.js","./index-6otl1p8d.js"])))=>i.map(i=>d[i]);
-import{_ as Sd}from"./iframe-YgFXYL1y.js";import{a as gd,r as Rn,R as ie}from"./index-6otl1p8d.js";import{r as bd,H as zd,A as od,e as Ad,D as Td}from"./index-DAjpRYvW.js";import"./index-Daw7QRpi.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";var Wc={exports:{}},ce={},$c={exports:{}},kc={};/**
+import{_ as Sd}from"./iframe-02KRTveA.js";import{a as gd,r as Rn,R as ie}from"./index-6otl1p8d.js";import{r as bd,H as zd,A as od,e as Ad,D as Td}from"./index-BH5fuBnn.js";import"./index-Daw7QRpi.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";var Wc={exports:{}},ce={},$c={exports:{}},kc={};/**
  * @license React
  * scheduler.production.js
  *
--- assets/Getting Started-D1e5Okr6.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as r}from"./index-P9OXKMel.js";import{S as s}from"./index-DAjpRYvW.js";import"./index-6otl1p8d.js";import"./iframe-YgFXYL1y.js";import"./index-Daw7QRpi.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";function e(n){const o={h1:"h1",h2:"h2",...r(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(o.h1,{id:"getting-started",children:"Getting Started"}),`
-`,t.jsx(o.h2,{id:"install",children:"Install"}),`
-`,t.jsx(s,{dark:!0,language:"bash",code:`
-// pnpm
-pnpm add @flightlesslabs/dodo-ui
-
-// npm
-npm i @flightlesslabs/dodo-ui
-`})]})}function u(n={}){const{wrapper:o}={...r(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{u as default};
