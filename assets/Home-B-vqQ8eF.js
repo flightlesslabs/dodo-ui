dd86f8353177fd6b8385e3f3519d647c22d7cd60
siddacool <sidthecool007@gmail.com>
2025-04-25 11:39:19 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@eae1f1fe7416e2afe672d1e92760198a031dd2c7 🚀

--- assets/Home-B-vqQ8eF.js
+++ assets/Home-B-vqQ8eF.js
@@ -1,6 +1,6 @@
-import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-P9OXKMel.js";import{a as t}from"./index-oteKnmY1.js";import"./index-6otl1p8d.js";import"./iframe-x09vIYKB.js";import"./index-Daw7QRpi.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";const r=`{
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{useMDXComponents as l}from"./index-P9OXKMel.js";import{a as t}from"./index-Bo1Aun5G.js";import"./index-6otl1p8d.js";import"./iframe-Cf5pLcJ8.js";import"./index-Daw7QRpi.js";import"./index-CfOrKyLd.js";import"./index-DrFu-skq.js";const r=`{
   "name": "@flightlesslabs/dodo-ui",
-  "version": "0.5.0",
+  "version": "0.6.0",
   "scripts": {
     "build": "vite build && pnpm run prepack",
     "preview": "vite preview",
