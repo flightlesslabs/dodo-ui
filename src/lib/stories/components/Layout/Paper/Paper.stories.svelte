1fe9003f75dd31b5ee79f0e25af6570b1d530c54
Siddhesh Mangela <sidthecool007@gmail.com>
2025-04-25 16:57:53 +0530

    shadow added

---
+++ src/lib/stories/components/Layout/Paper/Paper.stories.svelte
@@ -0,0 +1,37 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Paper from './Paper.svelte';
+  import type { StoryBookArgTypes } from '$lib/storybook-types.js';
+
+  export const storyPaperArgTypes: StoryBookArgTypes = {
+    roundness: {
+      control: { type: 'select' },
+      options: [false, '1x', '2x', '3x', 'full'],
+    },
+    shadow: {
+      control: { type: 'select' },
+      options: [false, '1x', '2x', '3x', '4x', '5x', '6x', '7x'],
+    },
+  };
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    component: Paper,
+    tags: ['autodocs'],
+    argTypes: storyPaperArgTypes,
+  });
+</script>
+
+<!-- ⚠️ Test Paper: Do not use! -->
+<Story
+  name="Test Paper"
+  args={{
+    _unsafeChildrenStringForTesting: 'Hola!',
+    _unsafePaddingForTesting: true,
+  }}
+/>
+
+<!-- Paper with default style -->
+<Story name="Default">
+  <Paper>Hola!</Paper>
+</Story>
---
+++ src/lib/stories/components/Layout/Paper/Shadow/Shadow.stories.svelte
@@ -0,0 +1,42 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Paper from '../Paper.svelte';
+  import { storyPaperArgTypes } from '../Paper.stories.svelte';
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    component: Paper,
+    tags: ['autodocs'],
+    argTypes: storyPaperArgTypes,
+  });
+</script>
+
+<Story name="Shadow 1x">
+  <Paper>Hola!</Paper>
+</Story>
+
+<Story name="Shadow 2x" args={{ shadow: '2x' }}><Paper shadow="2x">Hola!</Paper></Story>
+
+<Story name="Shadow 3x" args={{ shadow: '3x' }}>
+  <Paper shadow="3x">Hola!</Paper>
+</Story>
+
+<Story name="Shadow 4x" args={{ shadow: '4x' }}>
+  <Paper shadow="4x">Hola!</Paper>
+</Story>
+
+<Story name="Shadow 5x" args={{ shadow: '5x' }}>
+  <Paper shadow="5x">Hola!</Paper>
+</Story>
+
+<Story name="Shadow 6x" args={{ shadow: '6x' }}>
+  <Paper shadow="6x">Hola!</Paper>
+</Story>
+
+<Story name="Shadow 7x" args={{ shadow: '7x' }}>
+  <Paper shadow="7x">Hola!</Paper>
+</Story>
+
+<Story name="Shadow False" args={{ shadow: false }}>
+  <Paper shadow={false}>Hola!</Paper>
+</Story>
