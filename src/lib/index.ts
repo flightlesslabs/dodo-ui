e3928db4ffe9eff98b3fd84d80ead4510207ed44
Siddhesh Mangela <sidthecool007@gmail.com>
2026-02-10 16:51:12 +0530

    beta 1 added

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -7,22 +7,22 @@
  */
 export { Button } from './components/Form/index.js';
 
-export type {
-  ButtonAsButtonProps,
-  ButtonAsAnchorProps,
-  ButtonProps,
-} from './components/Form/index.js';
+// export type {
+//   ButtonAsButtonProps,
+//   ButtonAsAnchorProps,
+//   ButtonProps,
+// } from './components/Form/index.js';
 
 /**
  * TextInput component and related types/events.
  */
 export { TextInput } from './components/Form/index.js';
 
-export type {
-  TextInputEvent,
-  TextInputFocusEvent,
-  TextInputProps,
-} from './components/Form/index.js';
+// export type {
+//   TextInputEvent,
+//   TextInputFocusEvent,
+//   TextInputProps,
+// } from './components/Form/index.js';
 
 /**
  * InputEnclosure component and related prop types.
