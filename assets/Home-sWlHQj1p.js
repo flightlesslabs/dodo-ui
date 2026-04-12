5c3e033e82bb5b1ee1dcb40129a3643f3ad50471
siddacool <sidthecool007@gmail.com>
2026-04-12 09:15:18 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@6f14c18cacc20316ab180b4df01111947747054a 🚀

--- assets/Home-sWlHQj1p.js
+++ assets/Home-sWlHQj1p.js
@@ -1,6 +1,7 @@
 import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-6sF1Ejqi.js";import{r as n}from"./react-C6wZJlw-.js";import{t as r}from"./mdx-react-shim-w1JW0gnZ.js";var i,a=e((()=>{i=`{
   "name": "@documentation/docs",
-  "version": "0.1.0",
+  "version": "0.1.2",
+  "private": true,
   "scripts": {
     "preview": "vite preview",
     "prepare": "svelte-kit sync || echo ''",
@@ -100,7 +101,7 @@ import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-6sF1Ejqi.js"
     "esm-env": "^1.2.2"
   }
 }
-`}));function o(e){let t={a:`a`,code:`code`,h1:`h1`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,pre:`pre`,strong:`strong`,ul:`ul`,...n(),...e.components};return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsxs)(`main`,{style:{textAlign:`center`},children:[(0,c.jsx)(`img`,{src:`dodo-circle-6.png`,style:{width:200,height:`auto`}}),(0,c.jsx)(t.h1,{id:`dodo-ui`,children:`Dodo UI`}),(0,c.jsx)(`h3`,{style:{fontWeight:400},children:(0,c.jsxs)(t.p,{children:[`Opinionated UI framework for Svelte, based on `,(0,c.jsx)(t.a,{href:`https://bits-ui.com/`,rel:`nofollow`,children:`Bits UI`})]})}),(0,c.jsxs)(t.h3,{id:`github---npm`,children:[(0,c.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui`,rel:`nofollow`,children:`Github`}),` - `,(0,c.jsx)(t.a,{href:`https://www.npmjs.com/package/@flightlesslabs/dodo-ui`,rel:`nofollow`,children:`npm`})]})]}),`
+`}));function o(e){let t={a:`a`,code:`code`,h1:`h1`,h2:`h2`,h3:`h3`,li:`li`,p:`p`,pre:`pre`,strong:`strong`,ul:`ul`,...n(),...e.components};return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsxs)(`main`,{style:{textAlign:`center`},children:[(0,c.jsx)(`img`,{src:`dodo-circle-6.png`,style:{width:200,height:`auto`}}),(0,c.jsx)(t.h1,{id:`dodo-ui`,children:`Dodo UI`}),(0,c.jsx)(`h3`,{style:{fontWeight:400},children:(0,c.jsxs)(t.p,{children:[`Opinionated UI framework for Svelte, based on `,(0,c.jsx)(t.a,{href:`https://bits-ui.com/`,rel:`nofollow`,children:`Bits UI`})]})}),(0,c.jsxs)(t.h3,{id:`github---npm---changelog`,children:[(0,c.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui`,rel:`nofollow`,children:`Github`}),` - `,(0,c.jsx)(t.a,{href:`https://www.npmjs.com/package/@flightlesslabs/dodo-ui`,rel:`nofollow`,children:`npm`}),` - `,(0,c.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/blob/main/packages/dodo-ui/CHANGELOG.md`,rel:`nofollow`,children:`Changelog`})]})]}),`
 `,(0,c.jsx)(`br`,{}),`
 `,(0,c.jsx)(`br`,{}),`
 `,(0,c.jsx)(t.h2,{id:`-installation`,children:`🚀 Installation`}),`
