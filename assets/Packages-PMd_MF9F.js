d2253762850d3affdf2144e346c9f74fc9f42d3c
siddacool <sidthecool007@gmail.com>
2026-04-16 10:46:57 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@952806dc2fb20ea506a7112111917f1b076b7dfb 🚀

--- assets/Packages-PMd_MF9F.js
+++ assets/Packages-PMd_MF9F.js
@@ -2,22 +2,22 @@ import{n as e}from"./chunk-BneVvdWh.js";import{t}from"./jsx-runtime-6sF1Ejqi.js"
 `,(0,o.jsx)(t.hr,{}),`
 `,(0,o.jsx)(t.h2,{id:`-core`,children:`🧱 Core`}),`
 `,(0,o.jsxs)(t.ul,{children:[`
-`,(0,o.jsxs)(t.li,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui`,rel:`nofollow`,children:`Dodo UI`})}),`\r
+`,(0,o.jsxs)(t.li,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui`,rel:`nofollow`,children:`Dodo UI`})}),`
 The core design system and component library for building consistent Svelte applications.`]}),`
 `]}),`
 `,(0,o.jsx)(t.h2,{id:`-addons`,children:`🧩 Addons`}),`
 `,(0,o.jsx)(t.p,{children:`Extra building blocks to supercharge your UI.`}),`
 `,(0,o.jsxs)(t.ul,{children:[`
 `,(0,o.jsxs)(t.li,{children:[`
-`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui-bits`,rel:`nofollow`,children:`dodo-ui-bits`})}),`\r
+`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui-bits`,rel:`nofollow`,children:`dodo-ui-bits`})}),`
 An extension for Dodo UI that provides advanced, accessible components powered by `,(0,o.jsx)(t.a,{href:`https://bits-ui.com/`,rel:`nofollow`,children:`Bits UI`}),` (Select, Checkbox)`]}),`
 `]}),`
 `,(0,o.jsxs)(t.li,{children:[`
-`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui-date`,rel:`nofollow`,children:`dodo-ui-date`})}),`\r
+`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui-date`,rel:`nofollow`,children:`dodo-ui-date`})}),`
 An extension for Dodo UI that provides accessible date and time components powered by by `,(0,o.jsx)(t.a,{href:`https://bits-ui.com/`,rel:`nofollow`,children:`Bits UI`}),` (DatePicker)`]}),`
 `]}),`
 `,(0,o.jsxs)(t.li,{children:[`
-`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui-numeric`,rel:`nofollow`,children:`dodo-ui-numeric`})}),`\r
+`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui-numeric`,rel:`nofollow`,children:`dodo-ui-numeric`})}),`
 An extension for Dodo UI that provides formatted numeric input components powered by `,(0,o.jsx)(t.a,{href:`https://github.com/pitis/svelte-number-format`,rel:`nofollow`,children:`svelte-number-format`}),`.`]}),`
 `]}),`
 `]}),`
@@ -25,11 +25,11 @@ An extension for Dodo UI that provides formatted numeric input components powere
 `,(0,o.jsx)(t.p,{children:`Small but mighty helpers you’ll reach for often.`}),`
 `,(0,o.jsxs)(t.ul,{children:[`
 `,(0,o.jsxs)(t.li,{children:[`
-`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/currency`,rel:`nofollow`,children:`@flightlesslabs/currency`})}),`\r
+`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/currency`,rel:`nofollow`,children:`@flightlesslabs/currency`})}),`
 A lightweight TypeScript library providing country and currency mappings, including ISO codes and symbols.`]}),`
 `]}),`
 `,(0,o.jsxs)(t.li,{children:[`
-`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/time-utils`,rel:`nofollow`,children:`@flightlesslabs/time-utils`})}),`\r
+`,(0,o.jsxs)(t.p,{children:[(0,o.jsx)(t.strong,{children:(0,o.jsx)(t.a,{href:`https://github.com/flightlesslabs/time-utils`,rel:`nofollow`,children:`@flightlesslabs/time-utils`})}),`
 Date and time utilities at your disposal.`]}),`
 `]}),`
 `]})]})}function a(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,o.jsx)(t,{...e,children:(0,o.jsx)(i,{...e})}):i(e)}var o;e((()=>{o=t(),r()}))();export{a as default};
