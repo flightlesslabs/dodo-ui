8c8069ef58a8f65ae2a561709c5aac6de162825c
siddacool <sidthecool007@gmail.com>
2026-04-21 13:56:01 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@d859aa7143addf4151a1c7bbbfc502c81db34844 🚀

---
+++ assets/Grid.stories-XJKLX323.js
@@ -0,0 +1,137 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{Mt as t,Ot as n,V as r,W as i,bt as a,f as o,it as s,rt as c,yt as l,z as u}from"./iframe-PS8e3rka.js";import{N as d,P as f,f as p,l as m,n as h,p as g,r as ee,t as te,u as _}from"./create-runtime-stories-CQklLuHl.js";function ne(e,t){a(t,!0);var r=ae(),o=c(r);b(o,{name:`Default`,children:(e,t)=>{var r=x(),a=c(r);_(a,{sm:4,children:(e,t)=>{n(),u(e,i(`Hello there`))},$$slots:{default:!0}});var o=s(a,2);_(o,{sm:4,children:(e,t)=>{n(),u(e,i(`Hello there`))},$$slots:{default:!0}});var l=s(o,2);_(l,{sm:4,children:(e,t)=>{n(),u(e,i(`Hello there`))},$$slots:{default:!0}});var d=s(l,2);_(d,{sm:4,children:(e,t)=>{n(),u(e,i(`Hello there`))},$$slots:{default:!0}}),_(s(d,2),{sm:4,children:(e,t)=>{n(),u(e,i(`Hello there`))},$$slots:{default:!0}}),u(e,r)},$$slots:{default:!0},parameters:{docs:{description:{story:`Basic 12 column grid`}},__svelteCsf:{rawCode:`<Grid {...args}>
+  <Column sm={4}>Hello there</Column>
+<Column sm={4}>Hello there</Column>
+<Column sm={4}>Hello there</Column>
+<Column sm={4}>Hello there</Column>
+<Column sm={4}>Hello there</Column>
+</Grid>`}}});var d=s(o,2);b(d,{name:`Example`,children:(e,t)=>{var n=E(),r=c(n);_(r,{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,S())},$$slots:{default:!0}})},$$slots:{default:!0}});var i=s(r,2);_(i,{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,C())},$$slots:{default:!0}})},$$slots:{default:!0}});var a=s(i,2);_(a,{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,w())},$$slots:{default:!0}})},$$slots:{default:!0}}),_(s(a,2),{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,T())},$$slots:{default:!0}})},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Grid {...args}>
+  <Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+</Grid>`}}});var f=s(d,2);b(f,{name:`Grid Row`,children:(e,t)=>{var n=F(),r=c(n);m(r,{children:(e,t)=>{var n=A(),r=c(n);_(r,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,D())},$$slots:{default:!0}})},$$slots:{default:!0}});var i=s(r,2);_(i,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,O())},$$slots:{default:!0}})},$$slots:{default:!0}}),_(s(i,2),{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,k())},$$slots:{default:!0}})},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0}}),m(s(r,2),{children:(e,t)=>{var n=P(),r=c(n);_(r,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,j())},$$slots:{default:!0}})},$$slots:{default:!0}});var i=s(r,2);_(i,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,M())},$$slots:{default:!0}})},$$slots:{default:!0}}),_(s(i,2),{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,N())},$$slots:{default:!0}})},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Grid {...args}>
+  <Row>
+<Column sm={6} md={4} lg={3}>
+  <Card color="primary">
+    <p>Hello there</p>
+  </Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+  <Card color="primary">
+    <p>Hello there</p>
+  </Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+  <Card color="primary">
+    <p>Hello there</p>
+  </Card>
+</Column>
+</Row>
+<Row>
+<Column sm={6} md={4} lg={3}>
+  <Card color="primary">
+    <p>Hello there</p>
+  </Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+  <Card color="primary">
+    <p>Hello there</p>
+  </Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+  <Card color="primary">
+    <p>Hello there</p>
+  </Card>
+</Column>
+</Row>
+</Grid>`}}});var p=s(f,2);b(p,{name:`Gap`,args:{gap:2},children:(e,t)=>{var n=z(),r=c(n);_(r,{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,I())},$$slots:{default:!0}})},$$slots:{default:!0}});var i=s(r,2);_(i,{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,ie())},$$slots:{default:!0}})},$$slots:{default:!0}});var a=s(i,2);_(a,{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,L())},$$slots:{default:!0}})},$$slots:{default:!0}}),_(s(a,2),{sm:4,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,R())},$$slots:{default:!0}})},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Grid {...args}>
+  <Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={4}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+</Grid>`}}});var h=s(p,2);b(h,{name:`Responsive`,children:(e,t)=>{var n=K(),r=c(n);_(r,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,B())},$$slots:{default:!0}})},$$slots:{default:!0}});var i=s(r,2);_(i,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,V())},$$slots:{default:!0}})},$$slots:{default:!0}});var a=s(i,2);_(a,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,H())},$$slots:{default:!0}})},$$slots:{default:!0}});var o=s(a,2);_(o,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,U())},$$slots:{default:!0}})},$$slots:{default:!0}});var l=s(o,2);_(l,{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,W())},$$slots:{default:!0}})},$$slots:{default:!0}}),_(s(l,2),{sm:6,md:4,lg:3,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,G())},$$slots:{default:!0}})},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Grid {...args}>
+  <Column sm={6} md={4} lg={3}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column sm={6} md={4} lg={3}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+</Grid>`}}}),b(s(h,2),{name:`Flex`,children:(e,t)=>{var n=X(),r=c(n);_(r,{md:`flex`,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,q())},$$slots:{default:!0}})},$$slots:{default:!0}});var i=s(r,2);_(i,{md:2,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,J())},$$slots:{default:!0}})},$$slots:{default:!0}}),_(s(i,2),{md:2,children:(e,t)=>{g(e,{color:`primary`,children:(e,t)=>{u(e,Y())},$$slots:{default:!0}})},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Grid {...args}>
+  <Column md="flex">
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column md={2}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+<Column md={2}>
+<Card color="primary">
+  <p>Hello there</p>
+</Card>
+</Column>
+</Grid>`}}}),u(e,r),l()}var re,v,y,b,x,S,C,w,T,E,D,O,k,A,j,M,N,P,F,I,ie,L,R,z,B,V,H,U,W,G,K,q,J,Y,X,ae,Z,oe,se,Q,ce,le,ue,$;e((()=>{t(),ee(),f(),o(),h(),re=`
+  a set of components for your basic "12 column grid" needs
+
+ \`\`\`ts
+ import { Grid, Column, Row } from '@flightlesslabs/dodo-ui';
+ \`\`\`
+ `,v={children:{table:{category:`API`,subcategory:`Base`}},class:{table:{category:`API`,subcategory:`Base`}},gap:{table:{category:`API`,subcategory:`Base`,defaultValue:{summary:`1`}}}},y={component:p,tags:[`autodocs`],argTypes:v,parameters:{docs:{description:{component:re}}}},{Story:b}=d(y),x=r(`<!> <!> <!> <!> <!>`,1),S=r(`<p>Hello there</p>`),C=r(`<p>Hello there</p>`),w=r(`<p>Hello there</p>`),T=r(`<p>Hello there</p>`),E=r(`<!> <!> <!> <!>`,1),D=r(`<p>Hello there</p>`),O=r(`<p>Hello there</p>`),k=r(`<p>Hello there</p>`),A=r(`<!> <!> <!>`,1),j=r(`<p>Hello there</p>`),M=r(`<p>Hello there</p>`),N=r(`<p>Hello there</p>`),P=r(`<!> <!> <!>`,1),F=r(`<!> <!>`,1),I=r(`<p>Hello there</p>`),ie=r(`<p>Hello there</p>`),L=r(`<p>Hello there</p>`),R=r(`<p>Hello there</p>`),z=r(`<!> <!> <!> <!>`,1),B=r(`<p>Hello there</p>`),V=r(`<p>Hello there</p>`),H=r(`<p>Hello there</p>`),U=r(`<p>Hello there</p>`),W=r(`<p>Hello there</p>`),G=r(`<p>Hello there</p>`),K=r(`<!> <!> <!> <!> <!> <!>`,1),q=r(`<p>Hello there</p>`),J=r(`<p>Hello there</p>`),Y=r(`<p>Hello there</p>`),X=r(`<!> <!> <!>`,1),ae=r(`<!> <!> <!> <!> <!> <!>`,1),ne.__docgen={data:[],name:`Grid.stories.svelte`},Z=te(ne,y),oe=[`Default`,`Example`,`GridRow`,`Gap`,`Responsive`,`Flex`],se={...Z.Default,tags:[`svelte-csf-v5`]},Q={...Z.Example,tags:[`svelte-csf-v5`]},ce={...Z.GridRow,tags:[`svelte-csf-v5`]},le={...Z.Gap,tags:[`svelte-csf-v5`]},ue={...Z.Responsive,tags:[`svelte-csf-v5`]},$={...Z.Flex,tags:[`svelte-csf-v5`]}}))();export{se as Default,Q as Example,$ as Flex,le as Gap,ce as GridRow,ue as Responsive,oe as __namedExportsOrder,y as default,v as storyGridArgTypes};
