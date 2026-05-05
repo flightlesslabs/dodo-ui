1b582239c9f1d2d77f5f870b9173265e44382909
Siddhesh Mangela <sidthecool007@gmail.com>
2026-05-05 20:33:30 +0530

    fix

---
+++ packages/dodo-ui-bits/src/lib/components/Modals/ModalManager/ModalManager.stories.svelte
@@ -0,0 +1,48 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import type { ArgTypes } from 'storybook/internal/csf';
+  import type { ModalManagerProps } from './ModalManager.svelte';
+  import ModalManager from './ModalManager.svelte';
+  import { Button } from '@flightlesslabs/dodo-ui';
+  import { modals } from './store/modals.svelte.ts';
+
+  // ------------------------------
+  // Storybook ArgTypes
+  // ------------------------------
+  export const storyModalManagerArgTypes: Partial<ArgTypes<ModalManagerProps>> = {
+    // ------------------------------
+    // Core
+    // ------------------------------
+    id: {
+      table: { category: 'API', subcategory: 'Base' },
+    },
+  };
+
+  // ------------------------------
+  // Storybook Meta
+  // ------------------------------
+  const { Story } = defineMeta({
+    component: ModalManager,
+    tags: ['autodocs'],
+    argTypes: storyModalManagerArgTypes,
+    parameters: {
+      layout: 'fullscreen',
+    },
+  });
+</script>
+
+<!-- ------------------------------ -->
+<!-- Stories -->
+<!-- ------------------------------ -->
+
+<Story name="Default" asChild>
+  <Button
+    onclick={() => {
+      modals.add('confirm', { description: 'Are you sure' });
+    }}
+  >
+    Add Modal
+  </Button>
+
+  <ModalManager />
+</Story>
