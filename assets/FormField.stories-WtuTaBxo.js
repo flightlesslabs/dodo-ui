f44aaa3678dffb242178cf4c11018d69a77c989a
siddacool <sidthecool007@gmail.com>
2026-05-06 11:19:00 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@ce88d63a72288617b6f3f742214663e4ca2e4f5b 🚀

--- assets/FormField.stories-WtuTaBxo.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{Pt as t,bt as n,f as r,xt as i}from"./iframe-xlnz60GK.js";import{B as a,R as o,i as s,n as c,o as l,r as u,t as d}from"./create-runtime-stories-Gb8E1ZlD.js";function f(e,t){i(t,!0),g(e,{name:`Defualt`,args:{label:`Name:`,for:`name`},children:(e,t)=>{a(e,{placeholder:`Type something…`,name:`name`})},$$slots:{default:!0},parameters:{docs:{description:{story:`------------------------------`}},__svelteCsf:{rawCode:`<FormField {...args}>
-  <TextInput placeholder="Type something…" name="name" />
-</FormField>`}}}),n()}var p,m,h,g,_,v,y;e((()=>{t(),s(),l(),r(),c(),p=`
- a form control for input tags and more.
-
- \`\`\`ts
- import { FormField } from '@flightlesslabs/dodo-ui';
- \`\`\`
-`,m={class:{table:{category:`API`,subcategory:`Base`}},label:{table:{category:`API`,subcategory:`Base`}},for:{table:{category:`API`,subcategory:`Base`}}},h={component:o,tags:[`autodocs`],argTypes:m,parameters:{docs:{description:{component:p}}}},{Story:g}=u(h),f.__docgen={data:[],name:`FormField.stories.svelte`},_=d(f,h),v=[`Defualt`],y={..._.Defualt,tags:[`svelte-csf-v5`]}}))();export{y as Defualt,m as FormFieldArgTypes,v as __namedExportsOrder,h as default};
