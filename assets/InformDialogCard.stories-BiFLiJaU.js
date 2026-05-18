a886fff132ab6cb05671a1de1d9a7e9ecd24833f
siddacool <sidthecool007@gmail.com>
2026-05-18 13:47:30 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@2ad50ce463f4eb8f6ed2c73caa755d75cf0711cc 🚀

---
+++ assets/InformDialogCard.stories-BiFLiJaU.js
@@ -0,0 +1,22 @@
+import{i as e}from"./preload-helper-Cs4UwXAW.js";import{It as t,W as n,bt as r,f as i,kt as a,xt as o,z as s}from"./iframe-CN-A37OI.js";import{K as c,k as l,n as u,q as d,r as f,t as p}from"./create-runtime-stories-CsHfEFu7.js";import{t as m,u as h}from"./dist-BWWKAt7v.js";function g(e,t){o(t,!0),b(e,{name:`Default`,asChild:!0,children:(e,t)=>{h(e,{title:`Modal title`,children:(e,t)=>{a(),s(e,n(`Hello there`))},$$slots:{default:!0}})},$$slots:{default:!0},parameters:{docs:{description:{story:`------------------------------`}},__svelteCsf:{rawCode:`<InformDialogCard title="Modal title">Hello there</InformDialogCard>`}}}),r()}var _,v,y,b,x,S,C;e((()=>{t(),d(),f(),m(),i(),u(),_=`
+
+InformDialogCard for customizing InformDialog
+
+## 🚀 Installation
+
+\`\`\`bash
+pnpm add bits-ui @flightlesslabs/dodo-ui-bits
+\`\`\`
+
+
+For full setup (styles, themes, configuration), see the [Getting Started](https://github.com/flightlesslabs/dodo-ui/tree/main/packages/dodo-ui-bits) guide.
+
+## ✨ Usage
+
+ \`\`\`ts
+
+ import { InformDialogCard } from '@flightlesslabs/dodo-ui-bits';
+
+ <InformDialogCard />
+ \`\`\`
+ `,v={class:{table:{category:`API`,subcategory:`Base`}},clearable:{control:{type:`boolean`},table:{category:`API`,subcategory:`Base`}},controlsAlignment:{control:{type:`select`},options:l,table:{category:`API`,subcategory:`Base`,defaultValue:{summary:`end`}}},title:{table:{category:`API`,subcategory:`Base`}},customTitle:{table:{category:`API`,subcategory:`Slots`}}},y={component:h,tags:[`autodocs`],argTypes:v,parameters:{docs:{description:{component:_}}}},{Story:b}=c(y),g.__docgen={data:[],name:`InformDialogCard.stories.svelte`},x=p(g,y),S=[`Default`],C={...x.Default,tags:[`svelte-csf-v5`]}}))();export{C as Default,S as __namedExportsOrder,y as default,v as storyInformDialogCardArgTypes};
