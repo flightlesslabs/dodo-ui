74a0e955c7dd52dd161421e9ccd62c35ee92cced
siddacool <sidthecool007@gmail.com>
2026-05-19 06:58:51 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@348405fd5df9da28be259c4da2fa0d0f64accd19 🚀

---
+++ assets/Group.stories-B5Y9kp8B.js
@@ -0,0 +1,61 @@
+import{i as e}from"./preload-helper-Cs4UwXAW.js";import{It as t,V as n,W as r,at as i,bt as a,f as o,it as s,kt as c,xt as l,z as u}from"./iframe-JnVjh8xl.js";import{H as d,K as f,M as p,W as m,n as h,q as g,r as _,t as v,y}from"./create-runtime-stories-B3SB7HIJ.js";function b(e,t){l(t,!0);var n=j(),o=s(n);w(o,{name:`Default`,children:(e,t)=>{var n=T(),a=s(n);m(a,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Uno`))},$$slots:{default:!0}});var o=i(a,2);m(o,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Dos`))},$$slots:{default:!0}}),m(i(o,2),{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Tres`))},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{docs:{description:{story:`------------------------------`}},__svelteCsf:{rawCode:`<Group {...args}>
+  <Button variant="text" outline>Uno</Button>
+<Button variant="text" outline>Dos</Button>
+<Button variant="text" outline>Tres</Button>
+</Group>`}}});var f=i(o,2);w(f,{name:`Attached`,args:{attached:!0},children:(e,t)=>{var n=E(),a=s(n);m(a,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Uno`))},$$slots:{default:!0}});var o=i(a,2);m(o,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Dos`))},$$slots:{default:!0}}),m(i(o,2),{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Tres`))},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Group {...args}>
+  <Button variant="text" outline>Uno</Button>
+<Button variant="text" outline>Dos</Button>
+<Button variant="text" outline>Tres</Button>
+</Group>`}}});var p=i(f,2);w(p,{name:`Fullwidth`,args:{fullWidth:!0,attached:!0,flex:!0},children:(e,t)=>{var n=D(),a=s(n);m(a,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Uno`))},$$slots:{default:!0}});var o=i(a,2);m(o,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Dos`))},$$slots:{default:!0}}),m(i(o,2),{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Tres`))},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Group {...args}>
+  <Button variant="text" outline>Uno</Button>
+<Button variant="text" outline>Dos</Button>
+<Button variant="text" outline>Tres</Button>
+</Group>`}}});var h=i(p,2);w(h,{name:`Outline`,args:{outline:!0},children:(e,t)=>{var n=O(),a=s(n);m(a,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Uno`))},$$slots:{default:!0}});var o=i(a,2);m(o,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Dos`))},$$slots:{default:!0}}),m(i(o,2),{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Tres`))},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Group {...args}>
+  <Button variant="text" outline>Uno</Button>
+<Button variant="text" outline>Dos</Button>
+<Button variant="text" outline>Tres</Button>
+</Group>`}}});var g=i(h,2);w(g,{name:`TextInputs`,args:{attached:!0},children:(e,t)=>{var n=k(),r=s(n);d(r,{placeholder:`type something`});var a=i(r,2);d(a,{placeholder:`type something`}),d(i(a,2),{placeholder:`type something`}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Group {...args}>
+  <TextInput placeholder="type something" />
+<TextInput placeholder="type something" />
+<TextInput placeholder="type something" />
+</Group>`}}}),w(i(g,2),{name:`Multi Button Container`,args:{gap:3},children:(e,t)=>{var n=A(),a=s(n);m(a,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var o=i(a,2);m(o,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var l=i(o,2);m(l,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var d=i(l,2);m(d,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var f=i(d,2);m(f,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var p=i(f,2);m(p,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var h=i(p,2);m(h,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var g=i(h,2);m(g,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var _=i(g,2);m(_,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var v=i(_,2);m(v,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var y=i(v,2);m(y,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var b=i(y,2);m(b,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var x=i(b,2);m(x,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var S=i(x,2);m(S,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var C=i(S,2);m(C,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var w=i(C,2);m(w,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var T=i(w,2);m(T,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var E=i(T,2);m(E,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var D=i(E,2);m(D,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var O=i(D,2);m(O,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var k=i(O,2);m(k,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var j=i(k,2);m(j,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var M=i(j,2);m(M,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var N=i(M,2);m(N,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var P=i(N,2);m(P,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var F=i(P,2);m(F,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var I=i(F,2);m(I,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var L=i(I,2);m(L,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var R=i(L,2);m(R,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var z=i(R,2);m(z,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var B=i(z,2);m(B,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}});var V=i(B,2);m(V,{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}}),m(i(V,2),{variant:`text`,outline:!0,children:(e,t)=>{c(),u(e,r(`Item`))},$$slots:{default:!0}}),u(e,n)},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Group {...args}>
+  <Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+<Button variant="text" outline>Item</Button>
+</Group>`}}}),u(e,n),a()}var x,S,C,w,T,E,D,O,k,A,j,M,N,P,F,I,L,R,z;e((()=>{t(),g(),_(),o(),h(),x=`
+ An input/button Group component .
+
+ \`\`\`ts
+ import { Group } from '@flightlesslabs/dodo-ui';
+ \`\`\`
+`,S={children:{table:{category:`API`,subcategory:`Base`}},class:{table:{category:`API`,subcategory:`Base`}},attached:{table:{category:`API`,subcategory:`Base`}},roundness:{control:{type:`select`},options:p,description:`Border radius token`,table:{category:`API`,subcategory:`Appearance`}},fullWidth:{control:{type:`boolean`},table:{category:`API`,subcategory:`Appearance`}},flex:{control:{type:`boolean`},table:{category:`API`,subcategory:`Appearance`}},gap:{table:{category:`API`,subcategory:`Appearance`}},outline:{table:{category:`API`,subcategory:`Appearance`}}},C={component:y,tags:[`autodocs`],argTypes:S,parameters:{docs:{description:{component:x}}}},{Story:w}=f(C),T=n(`<!> <!> <!>`,1),E=n(`<!> <!> <!>`,1),D=n(`<!> <!> <!>`,1),O=n(`<!> <!> <!>`,1),k=n(`<!> <!> <!>`,1),A=n(`<!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!> <!>`,1),j=n(`<!> <!> <!> <!> <!> <!>`,1),b.__docgen={data:[],name:`Group.stories.svelte`},M=v(b,C),N=[`Default`,`Attached`,`Fullwidth`,`Outline`,`TextInputs`,`MultiButtonContainer`],P={...M.Default,tags:[`svelte-csf-v5`]},F={...M.Attached,tags:[`svelte-csf-v5`]},I={...M.Fullwidth,tags:[`svelte-csf-v5`]},L={...M.Outline,tags:[`svelte-csf-v5`]},R={...M.TextInputs,tags:[`svelte-csf-v5`]},z={...M.MultiButtonContainer,tags:[`svelte-csf-v5`]}}))();export{F as Attached,P as Default,I as Fullwidth,z as MultiButtonContainer,L as Outline,R as TextInputs,N as __namedExportsOrder,C as default,S as storyGroupArgTypes};
