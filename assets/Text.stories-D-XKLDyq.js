3b7900861aebf73c89965d3158d2c42969d38418
siddacool <sidthecool007@gmail.com>
2026-05-19 11:16:31 +0000

    Deploying to gh-pages from @ flightlesslabs/dodo-ui@e48a4b1ad55071b2e164b2b846ebf1d329631299 🚀

---
+++ assets/Text.stories-D-XKLDyq.js
@@ -0,0 +1,19 @@
+import{i as e}from"./preload-helper-Cs4UwXAW.js";import{It as t,V as n,W as r,at as i,bt as a,f as o,it as s,kt as c,xt as l,z as u}from"./iframe-Cvi8vf-1.js";import{K as d,c as f,g as p,n as m,q as h,r as g,s as _,t as v}from"./create-runtime-stories-1j-U1JW1.js";function y(e,t){l(t,!0);var n=w(),o=s(n);C(o,{name:`Default`,children:(e,t)=>{c(),u(e,r(`Hola! amigo`))},$$slots:{default:!0},parameters:{docs:{description:{story:`------------------------------`}},__svelteCsf:{rawCode:`<Text {...args}>
+  Hola! amigo
+</Text>`}}});var d=i(o,2);C(d,{name:`Primary`,args:{color:`primary`},children:(e,t)=>{c(),u(e,r(`Hola! amigo`))},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Text {...args}>
+  Hola! amigo
+</Text>`}}});var f=i(d,2);C(f,{name:`Danger`,args:{color:`danger`},children:(e,t)=>{c(),u(e,r(`Hola! amigo`))},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Text {...args}>
+  Hola! amigo
+</Text>`}}});var m=i(f,2);C(m,{name:`Safe`,args:{color:`safe`},children:(e,t)=>{c(),u(e,r(`Hola! amigo`))},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Text {...args}>
+  Hola! amigo
+</Text>`}}});var h=i(m,2);C(h,{name:`Light Theme`,asChild:!0,children:(e,t)=>{p(e,{type:`light`,children:(e,t)=>{_(e,{color:`primary`,children:(e,t)=>{c(),u(e,r(`Hola! amigo`))},$$slots:{default:!0}})},$$slots:{default:!0}})},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Theme type="light">
+  <Text color="primary">Hola! amigo</Text>
+</Theme>`}}}),C(i(h,2),{name:`Dark Theme`,asChild:!0,globals:{backgrounds:{value:`dark`}},children:(e,t)=>{p(e,{type:`dark`,children:(e,t)=>{_(e,{color:`primary`,children:(e,t)=>{c(),u(e,r(`Hola! amigo`))},$$slots:{default:!0}})},$$slots:{default:!0}})},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Theme type="dark">
+  <Text color="primary">Hola! amigo</Text>
+</Theme>`}}}),u(e,n),a()}var b,x,S,C,w,T,E,D,O,k,A,j,M;e((()=>{t(),h(),g(),o(),m(),b=`
+ A typography component with color option.
+
+ \`\`\`ts
+ import { Text } from '@flightlesslabs/dodo-ui';
+ \`\`\`
+`,x={children:{table:{category:`API`,subcategory:`Base`}},class:{table:{category:`API`,subcategory:`Base`}},color:{control:{type:`select`},options:f,description:`Color theme token`,table:{category:`API`,subcategory:`Appearance`,defaultValue:{summary:`default`}}}},S={component:_,tags:[`autodocs`],argTypes:x,parameters:{docs:{description:{component:b}}}},{Story:C}=d(S),w=n(`<!> <!> <!> <!> <!> <!>`,1),y.__docgen={data:[],name:`Text.stories.svelte`},T=v(y,S),E=[`Default`,`Primary`,`Danger`,`Safe`,`LightTheme`,`DarkTheme`],D={...T.Default,tags:[`svelte-csf-v5`]},O={...T.Primary,tags:[`svelte-csf-v5`]},k={...T.Danger,tags:[`svelte-csf-v5`]},A={...T.Safe,tags:[`svelte-csf-v5`]},j={...T.LightTheme,tags:[`svelte-csf-v5`]},M={...T.DarkTheme,tags:[`svelte-csf-v5`]}}))();export{k as Danger,M as DarkTheme,D as Default,j as LightTheme,O as Primary,A as Safe,E as __namedExportsOrder,S as default,x as storyTextArgTypes};
