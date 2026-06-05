41516f1137bb35b31ec50ee146c6fec9c82457d4
Siddhesh Mangela <sidthecool007@gmail.com>
2026-06-05 15:05:57 +0530

    Update ControlBox.svelte

--- documentation/customize/src/lib/components/ControlBox/ControlBox.svelte
+++ documentation/customize/src/lib/components/ControlBox/ControlBox.svelte
@@ -1,16 +1,40 @@
 <script>
-  import { Card } from '@flightlesslabs/dodo-ui';
+  import { Button, Card } from '@flightlesslabs/dodo-ui';
   import DodoLogo from './DodoLogo.svelte';
   import { Accordion } from '@flightlesslabs/dodo-ui-bits';
   import PageTitle from '../PageTitle.svelte';
   import Colors from './Colors.svelte';
   import Spacing from './Spacing.svelte';
+  import Icon from '@iconify/svelte';
+  import { useResetStore } from '$lib/store/reset.svelte';
+  import { useSpaceStore } from '$lib/store/space.svelte';
+  import { useSizeStore } from '$lib/store/size.svelte';
+  import { useRoundnessStore } from '$lib/store/roundness.svelte';
+  import { useColorStore } from '$lib/store/colors.svelte';
+
+  let anyFilterActive = $derived(
+    useRoundnessStore.isActive ||
+      useSizeStore.isActive ||
+      useSpaceStore.isActive ||
+      useColorStore.isActive,
+  );
 </script>
 
 <aside class="ControlBox">
   <Card class="ControlBoxCard" shadow={2}>
     <header>
       <DodoLogo />
+      <Button
+        compact
+        roundness="full"
+        variant="text"
+        onclick={() => useResetStore.reset()}
+        class="ResetButton"
+        aria-label="Reset"
+        disabled={!anyFilterActive}
+      >
+        <Icon icon="material-symbols:refresh-rounded" />
+      </Button>
     </header>
     <div class="title">
       <PageTitle>Customize</PageTitle>
@@ -46,6 +70,13 @@
 
   header {
     margin-bottom: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    :global(.ResetButton) {
+      font-size: 1.3rem;
+    }
   }
 
   .title {
