9e73ec90901cca19107336244d279d31390313b7
Siddhesh Mangela <sidthecool007@gmail.com>
2026-06-08 12:02:09 +0530

    Customize removed from homepage app

--- documentation/homepage/CHANGELOG.md
+++ documentation/homepage/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @documentation/homepage
 
+## 0.2.0
+
+### Minor Changes
+
+- Customize removed from homepage app
+
 ## 0.1.4
 
 ### Patch Changes
--- documentation/homepage/src/components/Customize/CustomizeTool/Preview/ui/Button.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { ButtonAsButtonProps } from '@flightlesslabs/dodo-ui';
-  import { Button as ButtonDodoUi } from '@flightlesslabs/dodo-ui';
-  import './Button.scss';
-
-  let { class: className = '', ...restProps }: ButtonAsButtonProps = $props();
-</script>
-
-<ButtonDodoUi {...restProps} class={['ButtonDodoUi', className].join(' ')} />
